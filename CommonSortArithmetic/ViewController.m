d59d3ed20a36750389b22da3d67db6a20b6b66a0
zhuzj <2603160657@qq.com>
2018-08-16 17:22:47 +0800

    快速排序OC 和 C 版本

--- CommonSortArithmetic/ViewController.m
+++ CommonSortArithmetic/ViewController.m
@@ -19,6 +19,9 @@
     // Do any additional setup after loading the view, typically from a nib.
     testBubbleSort();
     testSelectionSort();
+    [self testQuickSort];
+    testQuickSort();
+    
 }
 
 
@@ -81,6 +84,105 @@ void testSelectionSort(){
     }
     
     printList(array,num);
+}
+
+void testQuickSort(){
+    
+    int array[10] = {55, 23, 93, 23, 4, 56, 1, 34, 11, 69};
+    int num = sizeof(array)/sizeof(int);
+    quickSortArray(array, 0, num - 1);
+    
+    printList(array,num);
+}
+
+void quickSortArray(int *array,int leftIndex,int rightIndex){
+    
+    if (leftIndex >= rightIndex) {
+        return;
+    }
+    
+    int i = leftIndex;
+    int j = rightIndex;
+    int key = array[i];
+    
+    //1.确定key（基准数）的位置
+    while (i < j) {
+        
+        //1.1从右向左扫[移动右指针]，找比基准数小的为止 (i位置赋值，j位置空出）
+        while (i < j && array[j] >= key) {
+            j--;
+        }
+        array[i] = array[j];
+        
+        ////1.2从左向右扫[移动左指针]，找比基准数大的为止 （j位置赋值，i位置空出）
+        while (i < j && array[i] <= key) {
+            i++;
+        }
+        array[j] = array[i];
+    }
+    
+    //1.3最终确定key（基准数）的位置
+    array[i] = key;
+    
+    /**** 递归排序 ***/
+    //排序基准数左边的
+    quickSortArray(array, leftIndex, i - 1);
+    //排序基准数右边的
+    quickSortArray(array, i + 1, rightIndex);
+}
+
+/*
+ 3、快速排序算法
+ 该方法的基本思想是：
+ 1．先从数列中取出一个数作为基准数。
+ 2．分区过程，将比这个数大的数全放到它的右边，小于或等于它的数全放到它的左边。
+ 3．再对左右区间重复第二步，直到各区间只有一个数。
+ */
+- (void)testQuickSort{
+    
+    NSMutableArray *arr = [[NSMutableArray alloc] initWithObjects:@(55), @(23),@(93),@(23),@(4),@(56),@(1),@(34),@(69),nil];
+    
+    [self quickSortArray:arr withLeftIndex:0 andRightIndex:arr.count - 1];
+    
+    NSLog(@"%@",arr);
+}
+- (void)quickSortArray:(NSMutableArray *)array withLeftIndex:(NSInteger)leftIndex andRightIndex:(NSInteger)rightIndex{
+    
+    if (leftIndex >= rightIndex) {//如果数组长度为0或1时返回
+        return;
+    }
+    
+    NSInteger i = leftIndex;
+    NSInteger j = rightIndex;
+    //记录比较基准数
+    NSInteger key = [array[i] integerValue];
+    
+    while (i < j) {
+        //1.先从右到左
+        /**** 首先从右边j开始查找比基准数小的值 ***/
+        while (i < j && [array[j] integerValue] >= key) {//如果比基准数大，继续查找
+            j--;
+        }
+        //如果比基准数小，则将查找到的小值调换到i的位置
+        array[i] = array[j];
+        
+        //2.从左到右
+        /**** 当在右边查找到一个比基准数小的值时，就从i开始往后找比基准数大的值 ***/
+        while (i < j && [array[i] integerValue] <= key) {//如果比基准数小，继续查找
+            i++;
+        }
+         //如果比基准数大，则将查找到的大值调换到j的位置
+        array[j] = array[i];
+    }
+    
+    //将基准数放到正确位置
+    array[i] = @(key);
+    
+    /**** 递归排序 ***/
+    //排序基准数左边的
+    [self quickSortArray:array withLeftIndex:leftIndex andRightIndex:i - 1];
+    //排序基准数右边的
+    [self quickSortArray:array withLeftIndex:i + 1 andRightIndex:rightIndex];
 }
 
 
